7f7f83232f0cf5527b5b742c62bec95eb9794598
Gonçalo Mano <goncalo.alves.mano@gmail.com>
2024-05-20 14:54:32 +0100

    NavBar

--- minecraft-server-finder/app/layout.tsx
+++ minecraft-server-finder/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { NavBar } from "@/components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,9 +21,26 @@ export default function RootLayout({
         <title>
           Minecraft Server Finder
         </title>
-
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" crossOrigin="anonymous" />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+          <NavBar />
+        {children}
+
+
+        <script
+          src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+          integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+          crossOrigin="anonymous"
+        ></script>
+
+        <script
+          src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+          integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+          crossOrigin="anonymous"
+        ></script>
+      </body>
     </html>
   );
 }
--- minecraft-server-finder/components/navbar.tsx
+++ minecraft-server-finder/components/navbar.tsx
@@ -1,8 +1,23 @@
 
 export const NavBar = () => {
     return(
-        <div>
-            Nav
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbar"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+        <div className="collapse navbar-collapse" id="navbar">
+            <div className="navbar-nav">
+                <a className="nav-item nav-link" id="home" href="home.html">Home</a>
+                <a className="nav-item nav-link" id="jogador" href="jogador.html">Jogador</a>
+                <a className="nav-item nav-link" id="lista" href="lista.html">Lista</a>
+            </div>
         </div>
+    </nav>
     )
 }
